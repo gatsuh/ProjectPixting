a1790955b3ed0a74560387d805cce94ba5bb5573
mesmit22 <mesmit22@gmail.com>
2015-11-23 19:22:20 -0500

    setting image view to default image on failure of try statement

--- Pixting/app/src/main/java/com/gatsuh/pixting/MainActivity.java
+++ Pixting/app/src/main/java/com/gatsuh/pixting/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
+//
         newPix = (Button)findViewById(R.id.btnnew);
         newPix.setOnClickListener(new View.OnClickListener() {
             @Override
