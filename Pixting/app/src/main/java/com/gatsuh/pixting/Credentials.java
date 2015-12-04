05328eaa8112455d7817302ca181fcaeec649bdb
mesmit22 <mesmit22@gmail.com>
2015-12-04 15:25:34 -0500

    12/4/15 commit app- parse portion still needs testing

---
+++ Pixting/app/src/main/java/com/gatsuh/pixting/Credentials.java
@@ -0,0 +1,20 @@
+package com.gatsuh.pixting;
+
+/**
+ * Created by mark on 11/30/2015.
+ */
+public class Credentials {
+    private String applicationId = "vcs0Eq7HyFQQ3QOeqORFWBY4FYm3T2NgEFu1odqV";
+    private String clientKey = "hJiiPYMf9Gpl6zMZlbFK7TUGF2yyjwwGpTo3yYhA";
+
+    public Credentials(){
+    }
+
+    public String getApplicationId() {
+        return applicationId;
+    }
+
+    public String getClientKey() {
+        return clientKey;
+    }
+}
--- Pixting/app/src/main/java/com/gatsuh/pixting/MainActivity.java
+++ Pixting/app/src/main/java/com/gatsuh/pixting/MainActivity.java
@@ -1,7 +1,6 @@
 package com.gatsuh.pixting;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -11,26 +10,38 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
     final int CAMERA_DATA = 0;
-    Button newPix, login;
+    Button newPix, login, register;
     Bitmap bmp;
     ImageView img;
+    ParseObject testObject = new ParseObject("TestObject");
+    ParseObject user = new ParseObject("UserCredentials");
+    Credentials mCredentials = new Credentials();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, mCredentials.getApplicationId(), mCredentials.getClientKey());
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -52,28 +63,90 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "New Pix activity started", Toast.LENGTH_SHORT).show();
                 try {
                     Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(i, CAMERA_DATA);
+                    //startActivityForResult(i, CAMERA_DATA);
                 }catch (Exception e){
                     e.printStackTrace();
                 }finally {
                     img.setImageResource(R.drawable.sliced_bread_award_test);
                 }
             }
-        });//
+        });
 
-        login = (Button)findViewById(R.id.btn_login);
-        login.setOnClickListener(new View.OnClickListener() {
+        register = (Button)findViewById(R.id.btn_register);
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dialog dialog = new Dialog(getApplicationContext());
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setTitle("Login");
+                dialog.setContentView(R.layout.login);
+                dialog.setCanceledOnTouchOutside(false);
+                dialog.
+                dialog.show();
+
+                final EditText EDT_UserName = (EditText)dialog.findViewById(R.id.edt_userName);
+                final EditText EDT_Password = (EditText)dialog.findViewById(R.id.edt_password);
+                final EditText EDT_New_UserName = (EditText)dialog.findViewById(R.id.edt_new_username);
+                final EditText EDT_New_Password = (EditText)dialog.findViewById(R.id.edt_new_password);
+                final EditText EDT_New_UserEmail = (EditText)dialog.findViewById(R.id.edt_new_useremail);
+
+                Button register = (Button)dialog.findViewById(R.id.btn_register);
+                Button login = (Button)dialog.findViewById(R.id.btn_login_dialog);
+
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!(EDT_UserName.equals(null) && !(EDT_Password.equals(null)))) {
+                            ParseUser.logInInBackground(EDT_UserName.getText().toString(),
+                                    EDT_Password.getText().toString(), new LogInCallback() {
+                                        @Override
+                                        public void done(ParseUser user, ParseException e) {
+                                            if (user != null){
+                                                Toast.makeText(getApplicationContext(),"You are logged in",
+                                                        Toast.LENGTH_SHORT).show();
+                                            }else{
+                                                Toast.makeText(getApplicationContext(),"You must register",
+                                                        Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                            );
+                        }
+                    }
+                });
+
+                register.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!(EDT_New_UserName.equals(null) && !(EDT_New_Password.equals(null)))){
+                            ParseUser user = new ParseUser();
+                            user.setUsername(EDT_New_UserName.getText().toString());
+                            user.setPassword(EDT_New_Password.getText().toString());
+                            user.setEmail(EDT_New_UserEmail.getText().toString());
+                        }else if (EDT_New_UserName.equals(null)){
+                            Toast.makeText(getApplicationContext(),"Enter a user name",Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(getApplicationContext(),"Enter a password",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        login = (Button)findViewById(R.id.btn_login);
+        if (Login()) {
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                /*Dialog dialog = new Dialog(getApplicationContext());
                 dialog.setTitle("LOGIN");
                 dialog.setCancelable(false);
                 dialog.setCanceledOnTouchOutside(false);
                 dialog.setContentView(R.layout.login);
-                dialog.show();
-
-            }
-        });
+                dialog.show();*/
+                    Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
         img = (ImageView)findViewById(R.id.mainimage);
     }
@@ -107,6 +180,15 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             bmp = (Bitmap)extras.get("data");
             img.setImageBitmap(bmp);
+            testObject.add("Image", bmp);
+            testObject.saveInBackground();
         }
+    }
+
+    public boolean Login(){
+        user.put("username", "mark");
+        user.put("password", "admin");
+        user.saveInBackground();
+        return true;
     }
 }
