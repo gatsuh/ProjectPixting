998da9ac34f97868577180b81aa263e704b45956
Mark Smith <mesmit22@louisville.edu>
2015-12-07 18:55:53 -0500

    Now the dialog has register capabilities

--- Pixting/app/src/main/java/com/gatsuh/pixting/RegisterDialogFragment.java
+++ Pixting/app/src/main/java/com/gatsuh/pixting/RegisterDialogFragment.java
@@ -3,6 +3,7 @@ package com.gatsuh.pixting;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,7 @@ import com.parse.SignUpCallback;
  * Created by mark on 12/7/2015.
  */
 public class RegisterDialogFragment extends DialogFragment {
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -26,8 +28,8 @@ public class RegisterDialogFragment extends DialogFragment {
         builder.setMessage("Login");
         View v = inflater.inflate(R.layout.register_dialog, null);
         builder.setView(v);
-        final EditText EDT_New_UserName = (EditText)v.findViewById(R.id.edt_username);
-        final EditText EDT_New_Password = (EditText)v.findViewById(R.id.edt_password);
+        final EditText EDT_New_UserName = (EditText) v.findViewById(R.id.edt_username);
+        final EditText EDT_New_Password = (EditText) v.findViewById(R.id.edt_password);
         builder.setPositiveButton("Register", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -41,9 +43,7 @@ public class RegisterDialogFragment extends DialogFragment {
                         public void done(ParseException e) {
                             if (e == null) {
                                 Log.d("Test", "It works");
-                                //Toast.makeText(MainActivity.this, "Signed up", Toast.LENGTH_SHORT).show();
                             } else {
-                                //Toast.makeText(MainActivity.this, "Sign up was unsuccessful", Toast.LENGTH_SHORT).show();
                                 Log.d("problem", e.toString());
                             }
                         }
@@ -57,36 +57,6 @@ public class RegisterDialogFragment extends DialogFragment {
                         dialog.cancel();
                     }
                 });
-                /*.setPositiveButton("Register", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (!(EDT_New_UserName.equals(null) && !(EDT_New_Password.equals(null)))){
-                                    ParseUser user = new ParseUser();
-                                    user.setUsername(EDT_New_UserName.getText().toString());
-                                    user.setPassword(EDT_New_Password.getText().toString());
-                                    //user.setEmail(EDT_New_UserEmail.getText().toString());
-                                    user.signUpInBackground(new SignUpCallback() {
-                                        @Override
-                                        public void done(ParseException e) {
-                                            if (e == null){
-                                                Log.d("Test","It works");
-                                                //Toast.makeText(MainActivity.this, "Signed up", Toast.LENGTH_SHORT).show();
-                                            }else {
-                                                //Toast.makeText(MainActivity.this, "Sign up was unsuccessful", Toast.LENGTH_SHORT).show();
-                                                Log.d("problem", e.toString());
-                                            }
-                                        }
-                                    });
-                            }
-                        }
-                    })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });*/
-
         return builder.create();
     }
 }
